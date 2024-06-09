2842740bbf2f70c31af689bd1fc93f1fd1c4a3e7
Theviyanthan Krishnamohan <theviyanthan@outlook.com>
2024-06-09 22:44:25 +0100

    update analysis

--- ipl_bbb/analysis.R
+++ ipl_bbb/analysis.R
@@ -22,7 +22,8 @@ data <- read.csv("data.csv") %>%
     mutate(bounce_pos_y = -1 * bounce_pos_y) %>%
     mutate(phase = ifelse(over < 4, "PP1", ifelse(over < 7, "PP2", ifelse(over < 12, "Early Middle", ifelse(over < 17, "Late Middle", "Death"))))) %>%
     separate(runs, c("runs", "extras"), sep = " ") %>%
-    mutate(bowler_runs = ifelse(is_leg_bye == TRUE | is_bye == TRUE, 0, ifelse(is_no_ball == TRUE, as.integer(runs) - 1, as.integer(runs))))
+    mutate(bowler_runs = ifelse(is_leg_bye == TRUE | is_bye == TRUE, 0, ifelse(is_no_ball == TRUE, as.integer(runs) - 1, as.integer(runs))))  %>%
+    mutate(length = ifelse(bounce_pos_x < 0, "Full toss", ifelse(bounce_pos_x < 2, "Yorker", ifelse(bounce_pos_x < 6, "Slot", ifelse(bounce_pos_x < 8, "Good length", ifelse(bounce_pos_x < 10, "Short of a good length", ifelse(bounce_pos_x < 12, "Short", "Bouncer")))))))
 
 
 data <- removeOutliers(data, "bounce_pos_x")
@@ -44,9 +45,6 @@ data <- removeOutliers(data, "bounce_angle")
 data <- removeOutliers(data, "swing")
 
 
-data  <- data %>%
-    mutate(length = ifelse(bounce_pos_x < 0, "Full toss", ifelse(bounce_pos_x < 2, "Yorker", ifelse(bounce_pos_x < 6, "Slot", ifelse(bounce_pos_x < 8, "Good length", ifelse(bounce_pos_x < 10, "Short of a good length", ifelse(bounce_pos_x < 12, "Short", "Bouncer")))))))
-
 mp <- data %>%
     filter(bowler_name == "MATHEESHA PATHIRANA")
 
@@ -62,75 +60,16 @@ mp2024 <- mp %>%
 nt  <-  data %>%
     filter(bowler_name == "NUWAN THUSHARA")
 
-
-ggplot() +
-    geom_point(data=data, aes(y = bounce_pos_x, x = bounce_pos_y), color="blue") +
-    geom_point(data = data, aes(y = crease_pos_x, x = crease_pos_y), color="red") +
-    geom_point(data = data, aes(y = release_pos_x, x = release_pos_y), color="green") +
-    geom_point(data = data, aes(y = stump_pos_x, x = stump_pos_y), color="#fac0ff") +
-    labs(title = "Pitch Map",
-                x = "Line",
-                y = "Length")
-
 fast <- data %>%
     filter(delivery_type == "Seam")
-
-fast_rhb <- fast %>%
-    filter(batsman_is_rhb == TRUE)
-
-fast_lhb <- fast %>%
-    filter(batsman_is_rhb == FALSE)
-
-ggplot(fast_lhb, aes(y = release_pos_z, x = release_pos_y, color = bowler_is_rhb)) +
-    geom_point() +
-    labs(
-        title = "Pitch Map",
-        x = "Line",
-        y = "Length"
-    )
-
-ggplot(data, aes(y = bounce_pos_x, x = bounce_pos_y, color=length)) +
-    geom_point() +
-    labs(title = "Pitch Map",
-                x = "Line",
-                y = "Length")
-
-ggplot(data, aes(y = crease_pos_x, x = crease_pos_y, color=batsman_is_rhb)) +
-    geom_point() +
-    labs(title = "Pitch Map",
-                x = "Line",
-                y = "Length")
 
 rfb <- data %>%
     filter(bowler_is_rhb == TRUE & delivery_type == "Seam")  %>%
     filter(bowler_name != "MATHEESHA PATHIRANA" & bowler_name != "NUWAN THUSHARA")
 rfb_over  <-  rfb  %>%
     filter(release_pos_y > 0)
-ggplot() +
-    geom_point(data = rfb_over, aes(y = release_pos_z, x = release_pos_y), color="red") +
-    labs(title = "Pitch Map",
-                x = "Line",
-                y = "Length")
 rfb_release_z  <- median(rfb_over$release_pos_z)
 rfb_release_y  <- median(rfb_over$release_pos_y)
-
-ggplot() +
-    geom_point(data = mp, aes(y = release_pos_z, x = release_pos_y), color="yellow") +
-    geom_point(data = data, aes(y = rfb_release_z, x = rfb_release_y), color="red") +
-    geom_point(data = nt, aes(y = release_pos_z, x = release_pos_y), color="blue") +
-    labs(title = "Pitch Map",
-                x = "Line",
-                y = "Length")
-
-ggplot() +
-    geom_point(data = mp2022, aes(y = release_pos_z, x = release_pos_y), color = "yellow") +
-    geom_point(data = mp2023, aes(y = release_pos_z, x = release_pos_y), color = "red") +
-    geom_point(data = mp2024, aes(y = release_pos_z, x = release_pos_y), color = "blue") +
-    labs(
-        title = "Pitch Map",
-        x = "Line",
-        y = "Length"
-    )
 
 nt_release_z <- median(nt$release_pos_z)
 nt_release_y <- median(nt$release_pos_y)
@@ -148,6 +87,7 @@ mp2024_release_z <- median(mp2024$release_pos_z)
 mp2024_release_y <- median(mp2024$release_pos_y)
 
 ggplot() +
+geom_point(data = data, aes(y = rfb_release_z, x = rfb_release_y), color="orange") +
     geom_point(data = mp, aes(y = mp_release_z, x = mp_release_y), color = "yellow") +
     geom_point(data = nt, aes(y = nt_release_z, x = nt_release_y), color = "red") +
     geom_point(data = mp2022, aes(y = mp2022_release_z, x = mp2022_release_y), color = "blue") +
@@ -158,10 +98,6 @@ ggplot() +
         x = "Line",
         y = "Length"
     )
-
-ntSwing  <-  nt  %>%
-    group_by(over)  %>%
-    summarise(swing = mean(swing), no = n())
 
 fastSwing  <-  fast  %>%
     group_by(over)  %>%
@@ -182,19 +118,6 @@ mp2023Swing <-  mp2023  %>%
 mp2024Swing <-  mp2024  %>%
     group_by(over)  %>%
     summarise(swing = mean(swing), no = n())
-
-ggplot() +
-    geom_line(data = ntSwing, aes(x = over, y = swing), color = "red") +
-    geom_line(data = fastSwing, aes(x = over, y = swing), color = "blue") +
-    geom_line(data = mpSwing, aes(x = over, y = swing), color = "yellow") +
-    geom_line(data = mp2022Swing, aes(x = over, y = swing), color = "green") +
-    geom_line(data = mp2023Swing, aes(x = over, y = swing), color = "purple") +
-    geom_line(data = mp2024Swing, aes(x = over, y = swing), color = "orange") +
-    labs(
-        title = "Swing",
-        x = "Over",
-        y = "Swing"
-    )
 
 ntSwingPhase  <-  nt  %>%
     group_by(phase)  %>%
@@ -220,141 +143,102 @@ mp2024SwingPhase <-  mp2024  %>%
     group_by(phase)  %>%
     summarise(swing = mean(swing), no = n())
 
-ggplot() +
-    geom_line(data = ntSwingPhase, aes(x = phase, y = swing), color = "red") +
-    geom_line(data = fastSwingPhase, aes(x = phase, y = swing), color = "blue") +
-    geom_line(data = mpSwingPhase, aes(x = phase, y = swing), color = "yellow") +
-    geom_line(data = mp2022SwingPhase, aes(x = phase, y = swing), color = "green") +
-    geom_line(data = mp2023SwingPhase, aes(x = phase, y = swing), color = "purple") +
-    geom_line(data = mp2024SwingPhase, aes(x = phase, y = swing), color = "orange") +
-    labs(
-        title = "Swing",
-        x = "Phase",
-        y = "Swing"
-        )
-
-mpLengths  <- mp  %>%
-    group_by(year, length)  %>%
-    summarise(no = n())
-
-mpYear <- mp %>%
-    group_by(year) %>%
-    summarise(no = n())
-
-mpLengthsPer <- merge(mpLengths, mpYear, by = "year")  %>%
-    mutate(per = no.x / no.y * 100)
-
-ntLengths  <- nt  %>%
-    group_by(length)  %>%
-    summarise(no = n(), per = n() / nrow(nt) * 100)
-
-ntLengthsPhase  <- nt  %>%
-    group_by(phase, length)  %>%
-    summarise(no = n(), per = n() / sum(phase) * 100)
-
-
-s  <- mp2024  %>%
-    filter(phase=="Early Middle" | phase == "Late Middle")  %>%
-    group_by(bowler_name)  %>%
-    summarise(no = n(), swing = mean(swing))
-
-mpSlower <- mp %>%
-    filter(ball_type == "OFF CUTTER" | ball_type == "Knuckleball" | ball_type == "SLOWER BALL")
-
-maxSlowMP <- max(mpSlower$release_speed)
-mp <- mp %>%
-    mutate(is_slower = ifelse(release_speed < maxSlowMP, 1, 0))
-mpRPLength  <-  mp  %>%
-    group_by(length, is_slower)  %>%
-    summarise(rp_z = mean(release_pos_z), rp_y = mean(release_pos_y))
-
-mpRPLength2024 <-  mp2024  %>%
-    group_by(length)  %>%
-    summarise(rp_z = mean(release_pos_z), rp_y = mean(release_pos_y))
-
-mpRPLength2023 <-  mp2023  %>%
-    group_by(length)  %>%
-    summarise(rp_z = mean(release_pos_z), rp_y = mean(release_pos_y))
-
-mpRPLength2022 <-  mp2022  %>%
-    group_by(length)  %>%
-    summarise(rp_z = mean(release_pos_z), rp_y = mean(release_pos_y))
-
-bounceMP  <-  mp  %>%
-    group_by(year)  %>%
-    summarise(bounce = mean(stump_pos_z))
-
-bounceMPLengths  <-  mp  %>%
-    group_by(year, length)  %>%
-    summarise(bounce = mean(stump_pos_z))
-
-ggplot()+
-    geom_point(data = mp2022, aes(x = bounce_pos_x, y = stump_pos_z), color="red") +
-    geom_point(data = mp2023, aes(x = bounce_pos_x, y = stump_pos_z), color="blue") +
-    geom_point(data = mp2024, aes(x = bounce_pos_x, y = stump_pos_z), color="green") +
-    labs(
-        title = "Release Point",
-        x = "Line",
-        y = "Length"
-    )
-
 mpNT  <- data  %>%
     filter(bowler_name == "MATHEESHA PATHIRANA" | bowler_name == "NUWAN THUSHARA")
 
-ggplot() +
-    geom_point(data = mpNT, aes(x = bowler_name, y = release_speed, color = bowler_name)) +
-    labs(
-        title = "Release Point",
-        x = "Line",
-        y = "Length"
-    )
-
-mpSlower  <- mp  %>%
-    filter(ball_type == "OFF CUTTER" | ball_type == "Knuckleball" | ball_type=="SLOWER BALL")
-
-maxSlowMP = max(mpSlower$release_speed)
-maxSlowNT = 78
 mpNT  <-  mpNT  %>%
-    mutate(is_slower = ifelse(bowler_name == "MATHEESHA PATHIRANA" & release_speed < maxSlowMP, 1, ifelse(bowler_name == "NUWAN THUSHARA" & release_speed < maxSlowNT, 1, 0)))
-fastSlot  <- fast  %>%
-    group_by(length)  %>%
-    summarise(rp_z = mean(stump_pos_z))
+    mutate(is_slower = ifelse(ball_type == "OFF CUTTER" | ball_type == "Knuckleball" | ball_type == "SLOWER BALL" | ball_type == "SLOW BOUNCER", 1, 0))
 
-mpFast  <-  mpNT  %>%
-    filter(is_slower == 0 & bowler_name == "MATHEESHA PATHIRANA")
-ggplot() +
-    geom_point(data = mpFast, aes(x=release_pos_z, y=swing, color=over)) +
-    labs(
-        title = "Release Point",
-        x = "Line",
-        y = "Length"
-    )
-cor(mpFast$release_pos_z, mpFast$swing)
+mpPhaseTotal <- mpNT %>%
+    filter(bowler_name == "MATHEESHA PATHIRANA") %>%
+    group_by(phase) %>%
+    summarise(no = n())
 
-ggplot() +
-    geom_point(data = mp, aes(x = release_pos_z, y = initial_angle)) +
-    labs(
-        title = "Release Point",
-        x = "Line",
-        y = "Length"
-    )
+mpPhaseStrat <- mpNT %>%
+    filter(bowler_name == "MATHEESHA PATHIRANA") %>%
+    group_by(phase, length, is_slower) %>%
+    summarise(no = n())
 
-cor(mp$initial_angle, mp$stump_pos_z)
+mpPhaseStart   <-  merge(mpPhaseTotal, mpPhaseStrat, by = "phase", all.x = TRUE) %>%
+    mutate(percent = no.y / no.x * 100) %>%
+    select(phase, length, is_slower, percent, no.y)
 
-mpia <- mp %>%
-    group_by(year, length) %>%
-    summarise(ia = mean(initial_angle))
+mpPhaseTotal2022 <- mpNT %>%
+    filter(bowler_name == "MATHEESHA PATHIRANA") %>%
+    filter(year == "2022") %>%
+    group_by(phase) %>%
+    summarise(no = n())
+mpPhaseStrat2022  <- mpNT  %>%
+    filter(year == "2022")  %>%
+    filter(bowler_name == "MATHEESHA PATHIRANA") %>%
+    group_by(phase, length, is_slower) %>%
+    summarise(percent = n() / nrow(mp2022) * 100, no = n())
 
-fia <- fast %>%
-    group_by(length) %>%
-    summarise(ia = mean(initial_angle))
+mpPhaseStrat2022 <- merge(mpPhaseTotal2022, mpPhaseStrat2022, by = "phase", all.x = TRUE) %>%
+    mutate(percent = no.y / no.x * 100) %>%
+    select(phase, length, is_slower, percent, no.y)
 
-ggplot() +
-    geom_point(data = mp, aes(x = release_speed, y = swing)) +
-    labs(
-        title = "Release Point",
-        x = "Line",
-        y = "Length"
-    )
+mpPhaseTotal2023 <- mpNT %>%
+    filter(bowler_name == "MATHEESHA PATHIRANA") %>%
+    filter(year == "2023") %>%
+    group_by(phase) %>%
+    summarise(no = n())
 
-cor(mp$release_speed, mp$swing)
+mpPhaseStart2023  <- mpNT  %>%
+    filter(year == "2023")  %>%
+    filter(bowler_name == "MATHEESHA PATHIRANA") %>%
+    group_by(phase, length, is_slower) %>%
+    summarise(percent = n() / nrow(mp2023) * 100, no = n())
+
+mpPhaseStrat2023 <- merge(mpPhaseTotal2023, mpPhaseStart2023, by = "phase", all.x = TRUE) %>%
+    mutate(percent = no.y / no.x * 100) %>%
+    select(phase, length, is_slower, percent, no.y)
+
+mpPhaseTotal2024 <- mpNT %>%
+    filter(bowler_name == "MATHEESHA PATHIRANA") %>%
+    filter(year == "2024") %>%
+    group_by(phase) %>%
+    summarise(no = n())
+mpPhaseStrat2024  <- mpNT  %>%
+    filter(year == "2024")  %>%
+    filter(bowler_name == "MATHEESHA PATHIRANA") %>%
+    group_by(phase, length, is_slower) %>%
+    summarise(percent = n() / nrow(mp2024) * 100, no = n())
+mpPhaseStrat2024 <- merge(mpPhaseTotal2024, mpPhaseStrat2024, by = "phase", all.x = TRUE) %>%
+    mutate(percent = no.y / no.x * 100) %>%
+    select(phase, length, is_slower, percent, no.y)
+
+ntPhaseTotal <- mpNT %>%
+    filter(bowler_name == "NUWAN THUSHARA") %>%
+    group_by(phase) %>%
+    summarise(no = n())
+ntPhaseStrat <- mpNT %>%
+    filter(bowler_name == "NUWAN THUSHARA") %>%
+    group_by(phase, length, is_slower) %>%
+    summarise(no = n())
+ntPhaseStrat <- merge(ntPhaseTotal, ntPhaseStrat, by = "phase", all.x = TRUE) %>%
+    mutate(percent = no.y / no.x * 100) %>%
+    select(phase, length, is_slower, percent, no.y)
+
+mpPace <- mean((mpNT %>%
+    filter(bowler_name == "MATHEESHA PATHIRANA"))$release_speed)
+
+mpPace2022 <- mean((mpNT %>%
+    filter(bowler_name == "MATHEESHA PATHIRANA" & year == "2022"))$release_speed)
+mpPace2023 <- mean((mpNT %>%
+    filter(bowler_name == "MATHEESHA PATHIRANA" & year == "2023"))$release_speed)
+mpPace2024 <- mean((mpNT %>%
+    filter(bowler_name == "MATHEESHA PATHIRANA" & year == "2024"))$release_speed)
+ntPace <- mean((mpNT %>%
+    filter(bowler_name == "NUWAN THUSHARA"))$release_speed)
+
+ntSwingingBalls <- nt %>%
+    filter(swing <= -1)
+
+ntSwingLessBalls <- nt %>%
+    filter(swing > -1)
+
+ntSwingRR <- sum(ntSwingingBalls$bowler_runs) / nrow(ntSwingingBalls)
+ntSwingLessRR <- sum(ntSwingLessBalls$bowler_runs) / nrow(ntSwingLessBalls)
+ntSwingSR  <- nrow(ntSwingingBalls) / nrow(ntSwingingBalls  %>% filter(is_bowler_wicket == TRUE))
+ntSwingLessSR  <- nrow(ntSwingLessBalls) / nrow(ntSwingLessBalls  %>% filter(is_bowler_wicket == TRUE))
